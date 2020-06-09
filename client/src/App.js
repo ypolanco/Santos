0cfdbd4ebd40f7a074a914588a21a84f5d9cb0b1
jp <y.polanco12@gmail.com>
2020-06-09 18:55:46 -0400

    front end login

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Login from "./components/Login";
+import { loginUser } from "./services/auth";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  state = {
+    currentUser: null,
+  };
+
+  handleLoginSubmit = async (loginData) => {
+    const currentUser = await loginUser(loginData);
+    this.setState({ currentUser });
+  };
+
+  render() {
+    return (
+      <div>
+        <Login handleLoginSubmit={this.props.handleLoginSubmit} />
+      </div>
+    );
+  }
 }
-
-export default App;
---
+++ client/src/services/securities.js
@@ -0,0 +1,26 @@
+import api from "./api-helper";
+
+export const getAllSecurities = async () => {
+  const resp = await api.get("/securities");
+  return resp.data;
+};
+
+export const getOneSecurity = async (id) => {
+  const resp = await api.get(`/securities/${id}`);
+  return resp.data;
+};
+
+export const createSecurities = async (securityData) => {
+  const resp = await api.post("/securities", { security: securityData });
+  return resp.data;
+};
+
+export const updateSecurities = async (id, portData) => {
+  const resp = await api.put(`/securities/${id}`, { security: securityData });
+  return resp.data;
+};
+
+export const deleteSecurities= async (id) => {
+  const resp = await api.delete(`/securities/${id}`);
+  return resp;
+};
