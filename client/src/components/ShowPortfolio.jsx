865faeda194aff95eca8dea04f6c861843d0e677
jp <y.polanco12@gmail.com>
2020-06-12 14:39:26 -0400

    main section somwhat done

--- client/src/components/ShowPortfolio.jsx
+++ client/src/components/ShowPortfolio.jsx
@@ -16,23 +16,24 @@ export default function ShowPortfolios(props) {
           <h3 className="section">Portfolios</h3>
 
           <Link to="/new/portfolio">
-            <button className="">Add Portfolio</button>
+            <p className="add-button"><span className="plus">+</span> Portfolio</p>
           </Link>
         </div>
-
+        <div className="portfolios">
         {portfolios.map((port) => (
-          <>
+          <div className="port">
             <Link to={`/portfolios/${port.id}/securities`} key={port.name}>
-              <p key={port.id}>{port.name}</p>
+              <p key={port.id} className="">{port.name}</p>
             </Link>
             <Link to={`/portfolios/${port.id}/edit`}>
-              <button> Edit Portfolio </button>
+              <p>Edit Portfolio</p>
             </Link>
             <button onClick={() => destroyPortfolio(port.id)}>
               Delete Portfolio
             </button>
-          </>
+          </div>
         ))}
+          </div>
       </div>
     </>
   );
