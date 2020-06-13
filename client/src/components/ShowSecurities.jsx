eaac5b222073e13c4110b3ac7c96652adb8b5d09
jp <y.polanco12@gmail.com>
2020-06-13 16:34:43 -0400

    security output putline done

--- client/src/components/ShowSecurities.jsx
+++ client/src/components/ShowSecurities.jsx
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import "./ShowSecurities.css";
 
 export default function ShowSecurities(props) {
   const [id, setId] = useState(props.match.match.params.id);
-  const [form, setForm] = useState({ticker: ""});
+  const [form, setForm] = useState({ ticker: "" });
   console.log(props);
 
   const {
@@ -35,42 +36,43 @@ export default function ShowSecurities(props) {
   };
 
   return (
-    <>
+    <div className="securities">
       <hr />
-      {/* {portfolios.map((port) => (
-        <h3>{port.name}</h3>
-      ))} */}
-      <h3>Securities</h3>
-      {securities.map((security) => (
-        <React.Fragment key={security.id}>
-          <h4>{security.ticker}</h4>
-          <p>{security.price}</p>
-          <p>{security.ftWH}</p>
-          <p>{security.ftWH}</p>
-          {currentUser && currentUser.id === security.portfolio_id && (
-            <>
-              <button>edit</button>
-              <button onClick={() => destroySecurities(security.id)}>
-                delete
-              </button>
-            </>
-          )}
-        </React.Fragment>
-      ))}
+      <div className="securities-header">
+        <h3>Securities</h3>
+        <form action="" onSubmit={handleSubmit}>
+          <input
+            type="text"
+            value={form.ticker}
+            onChange={(e) =>
+              setForm({ ticker: e.target.value, portfolio_id: id })
+            }
+          />
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+      
+        {securities.map((security) => (
+          <React.Fragment key={security.id}>
+            <div className="securities-display">
+            <h4>Ticker: {security.ticker}</h4>
+            <p>Current Price:{security.price}</p>
+            <p>52 Week High: {security.ftWH}</p>
+            <p>52 Week Low: {security.ftWH}</p>
+            {currentUser && currentUser.id === security.portfolio_id && (
+              <>
+                <button>edit</button>
+                <button onClick={() => destroySecurities(security.id)}>
+                  delete
+                </button>
+              </>
+              )}
+              </div>
+          </React.Fragment>
+        ))}
+      
       {/*  */}
-      <form action="" onSubmit={handleSubmit}>
-        <input
-          type="text"
-          value={form.ticker}
-          onChange={(e) =>
-            setForm({ ticker: e.target.value, portfolio_id: id })
-          }
-        />
-        <button type="submit">Submit</button>
-        {/* update state in this file  */}
-      </form>
       <br />
-      {/* <Link to='/new/food'><button>Create</button></Link> */}
-    </>
+    </div>
   );
 }
