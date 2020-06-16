b6aaeec86be817c87bdbc78463283479f69ea5ef
jp <y.polanco12@gmail.com>
2020-06-16 10:26:56 -0400

    updated seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,3 @@
-# frozen_string_literal: true
-
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
 
 Portfolio.destroy_all
 Security.destroy_all
@@ -80,16 +71,3 @@ Security.create!(portfolio_id: @port3.id, ticker: 'FILA', price: 1000, ftWH: 120
 
 p "#{Security.count} security(s) created"
 
-# Portfolio.destroy_all
-# Security.destroy_all
-# User.destroy_all
-
-# @user = User.create!({ username: 'jp', email: 'yampi@email.com', password: '123456' })
-
-# p "#{User.count} user(s) created"
-# @apple = Security.create!(portfolio: @port, ticker: 'AAPL', price: 100, ftWH: 120, ftWL: 90, purchase_price: 98, position_size: 2)
-# p "#{Security.count} security(s) created"
-
-# @port = Portfolio.create!(name: 'Port1', user: @user, security: @apple)
-
-# p "#{Portfolio.count} porfolio(s) created"
